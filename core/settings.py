6da6e3b8b7044e0612a22d62f96ab7bd139c8499
phonglex <fonglex@gmail.com>
2021-06-26 21:30:00 +0700

    fix setting

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-practice-phong.herokuapp.com']
+ALLOWED_HOSTS = ['blog-practice-phong.herokuapp.com', '*']
 
 
 # Application definition
