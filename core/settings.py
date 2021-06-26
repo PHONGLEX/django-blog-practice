02c17406731299e9adcb24d6d2c52643784f1440
phonglex <fonglex@gmail.com>
2021-06-26 21:20:10 +0700

    fix whitenoise

--- core/settings.py
+++ core/settings.py
@@ -169,7 +169,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
